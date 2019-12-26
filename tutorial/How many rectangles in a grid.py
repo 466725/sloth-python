7a5c146b33004726e972663181e5bdc89aafe45b
Tianle Zheng <tianle.zheng.ca@gmail.com>
2019-12-25 21:08:11 -0500

    Remove comments

--- tutorial/How many rectangles in a grid.py
+++ tutorial/How many rectangles in a grid.py
@@ -2,11 +2,60 @@ w = int(input('Insert the width of the grid.'))
 h = int(input('Insert the height of the grid.'))
 b = 0
 for i in range(1, h + 1):
-    for j in range(1,w + 1):
+    for j in range(1, w + 1):
         a = i * j
         b += a
 print(b)
-# I am Bob :)
-# I am Fred ( * - * )
-# I am Sventha <(^-^)>
-# I am Olaf and I like warm hugs (*|*)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
