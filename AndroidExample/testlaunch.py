782f8c668b637b42b034bd4043ae8c756ea0a127
gleung <glory.leung@cineplex.com>
2020-04-01 11:52:22 -0400

    added starting emulator from command line

--- AndroidExample/testlaunch.py
+++ AndroidExample/testlaunch.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import unittest
 from appium import webdriver
 from time import sleep
@@ -8,6 +9,13 @@ from Resources.TestData import TestData
 
 class LaunchCineplexTests(unittest.TestCase):
     "Class to run tests against Cineplex app"
+
+    @classmethod
+    def setUpClass(cls):
+        "Launch emulator"
+        subprocess.Popen("emulator -avd Nexus_5X")
+        sleep(15)
+
     def setUp(self):
         "Setup for the test"
         desired_caps = {}
@@ -22,22 +30,29 @@ class LaunchCineplexTests(unittest.TestCase):
         desired_caps['newCommandTimeout'] = 120
         self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 
-    def tearDown(self):
-        "Tear down test and uninstall the application from the device"
-        self.driver.remove_app('com.fivemobile.cineplex.uat')
-
-    def test_launch_and_login_then_logout(self):
+    def test_launch_and_login(self):
         "Test that the app launched and user can log in and then logout"
-        self.driver.find_element_by_xpath("(//android.view.ViewGroup[@content-desc='NO THANKS'])").click()
+        sleep(5)
+        self.driver.find_element_by_xpath("(//android.view.ViewGroup[@content-desc='NO THANKS'])[2]").click()
         self.driver.find_element_by_xpath("//android.view.ViewGroup[@content-desc='Account']").click()
         self.driver.find_element_by_xpath("//android.view.ViewGroup[@content-desc='LOGIN']").click()
         self.driver.find_element_by_xpath("(//android.widget.EditText)[1]").send_keys(TestData.USERNAME)
         self.driver.find_element_by_xpath("(//android.widget.EditText)[2]").send_keys(TestData.PASSWORD)
         self.driver.find_element_by_xpath("//android.view.ViewGroup[@content-desc='LOGIN']").click()
         "User is logged in"
-        sleep(30)
+        sleep(10)
+
+    def tearDown(self):
+        "Tear down test and uninstall the application from the device"
+        self.driver.remove_app('com.fivemobile.cineplex.uat')
+
+    @classmethod
+    def tearDownClass(cls):
+        "kill emulator"
+        #subprocess.Popen('adb -s emulator-5554 emu kill')
 
 #---START OF SCRIPT
 if __name__ == '__main__':
+
     suite = unittest.TestLoader().loadTestsFromTestCase(LaunchCineplexTests)
     unittest.TextTestRunner(verbosity=2).run(suite)
