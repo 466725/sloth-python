74d484c23379c0c23143028949ae6d022216b4f7
gleung <glory.leung@cineplex.com>
2020-04-02 20:49:15 -0400

    script changes

---
+++ AndroidExample/testsaucelabs.py
@@ -0,0 +1,39 @@
+import unittest
+from appium import webdriver
+from time import sleep
+
+class LaunchCineplexTests(unittest.TestCase):
+    "Class to run tests against Cineplex app"
+
+    def setUp(self):
+        "Setup for the test"
+        desired_caps = {}
+        desired_caps['testobject_api_key'] = '73641AF4E4F34C13A31972953A7E27F0'
+        desired_caps['platformName'] = 'Android'
+        desired_caps['platformVersion'] = '10'
+        testobject_endpoint = 'http://us1.appium.testobject.com/wd/hub'
+        self.driver = webdriver.Remote(testobject_endpoint, desired_caps)
+        print(self.driver.desired_capabilities) # for DEBUG
+
+    def test_launch_and_login(self):
+        "Test that the app launched and user can log in and then logout"
+        # sleep(2)
+        self.driver.find_element_by_xpath("(//android.view.ViewGroup[@content-desc='NO THANKS'])[2]").click()
+        # sleep(2)
+        self.driver.find_element_by_xpath("//android.view.ViewGroup[@content-desc='Account']").click()
+        # sleep(2)
+        self.driver.find_element_by_xpath("//android.view.ViewGroup[@content-desc='LOGIN']").click()
+        # sleep(2)
+        self.driver.find_element_by_xpath("(//android.widget.EditText)[1]").send_keys("cpxapitester@gmail.com")
+        self.driver.find_element_by_xpath("(//android.widget.EditText)[2]").send_keys("Cineplex123")
+        self.driver.find_element_by_xpath("//android.view.ViewGroup[@content-desc='LOGIN']").click()
+        "User is logged in"
+        self.assertEqual(1+1, 2)
+        sleep(10)
+
+    def tearDown(self):
+        self.driver.quit()
+
+#---START OF SCRIPT
+if __name__ == '__main__':
+    unittest.main()
