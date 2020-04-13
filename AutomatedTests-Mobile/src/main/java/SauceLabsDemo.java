6c2c3a31f37847f6ecec9a8698a81ed37ad3ece5
skywolf91map <weipeng.zheng.ca@gmail.com>
2020-04-13 14:13:49 -0400

    Support IOS

--- AutomatedTests-Mobile/src/main/java/SauceLabsDemo.java
+++ AutomatedTests-Mobile/src/main/java/SauceLabsDemo.java
@@ -21,23 +21,39 @@ public class SauceLabsDemo {
 		androidCaps.setCapability("testobject_app_id", "1");
 		androidCaps.setCapability("platformName", "Android");
 		androidCaps.setCapability("platformVersion", "10");
+		DesiredCapabilities iosCaps = new DesiredCapabilities();
+		iosCaps.setCapability("testobject_api_key", "7B0F153A1C564E13934E7C5908228326");
+		iosCaps.setCapability("testobject_app_id", "1");
+		iosCaps.setCapability("platformName", "iOS");
+		iosCaps.setCapability("platformVersion", "11.0");
 		URL US_endpoint = new URL("http://us1.appium.testobject.com/wd/hub");
 		androidDriver = new AndroidDriver<WebElement>(US_endpoint, androidCaps);
-		iosDriver = new IOSDriver<WebElement>(US_endpoint, androidCaps);
+		iosDriver = new IOSDriver<WebElement>(US_endpoint, iosCaps);
 	}
 
 	@AfterSuite
 	public void afterSuite() throws InterruptedException {
 		androidDriver.quit();
+		iosDriver.quit();
 	}
 
 	@Test(enabled = true)
-	public void loginCineplex() throws InterruptedException {
+	public void androidLogin() throws InterruptedException {
 		androidDriver.findElementByXPath("(//android.view.ViewGroup[@content-desc='NO THANKS'])[2]").click();
 		androidDriver.findElementByXPath("//android.view.ViewGroup[@content-desc='Account']").click();
 		androidDriver.findElementByXPath("//android.view.ViewGroup[@content-desc='LOGIN']").click();
 		androidDriver.findElementByXPath("(//android.widget.EditText)[1]").sendKeys("cpxapitester@gmail.com");
 		androidDriver.findElementByXPath("(//android.widget.EditText)[2]").sendKeys("Cineplex123");
 		androidDriver.findElementByXPath("//android.view.ViewGroup[@content-desc='LOGIN']").click();
+	}
+	
+	@Test(enabled = true)
+	public void iosLogin() throws InterruptedException {
+		iosDriver.findElementByXPath("(//android.view.ViewGroup[@content-desc='NO THANKS'])[2]").click();
+		iosDriver.findElementByXPath("//android.view.ViewGroup[@content-desc='Account']").click();
+		iosDriver.findElementByXPath("//android.view.ViewGroup[@content-desc='LOGIN']").click();
+		iosDriver.findElementByXPath("(//android.widget.EditText)[1]").sendKeys("cpxapitester@gmail.com");
+		iosDriver.findElementByXPath("(//android.widget.EditText)[2]").sendKeys("Cineplex123");
+		iosDriver.findElementByXPath("//android.view.ViewGroup[@content-desc='LOGIN']").click();
 	}
 }
