cc8080b7b2310973c4d2b7a4c5ef6544050d52f3
skywolf91map <weipeng.zheng.ca@gmail.com>
2020-04-19 14:20:21 -0400

    Functioning

--- SauceLabs/Android/SauceLabsAndroidDemo.py
+++ SauceLabs/Android/SauceLabsAndroidDemo.py
@@ -1,29 +1,43 @@
 import unittest
-import pytest
 from appium import webdriver
 from time import sleep
 
+
 class AndroidApplicationTests(unittest.TestCase):
-    "Class to run tests against Cineplex app"
 
     def setUp(self):
-        "Setup for the test"
-        desired_caps = {'testobject_api_key': '9C862FE0E2624DD2BAC1B293A753A63B'
+        # 9BF029C703084859B7A85F653A502A87 -> Ticketing PROD
+        # 9C862FE0E2624DD2BAC1B293A753A63B -> Ticketing UAT
+        # EB0105D50CF04B7E9E6F7A514B4A1F60 -> Store PROD
+        # A37A095D95FD4A38B6FFA7258BC63BB3 -> Store UAT
+        desired_caps = {'testobject_api_key': 'EB0105D50CF04B7E9E6F7A514B4A1F60'
             , 'platformName': 'Android'
             , 'platformVersion': '9'
-            , 'automationName': 'UiAutomator2'
+            , 'deviceName': 'Huawei P30'
+            , 'phoneOnly': 'true'
+                        # , 'tabletOnly': 'false'
+                        # , 'privateDevicesOnly': 'false'
+                        # Optional Capabilities:
+                        # , 'appiumVersion': '1.17.0'
+                        # , 'noReset': 'false'
+                        # , 'cacheId': '1719330c118'
+                        # , 'testobject_session_creation_timeout': '900000'
+                        # , 'testobject_app_id': '1'
+                        # , 'testobject_suite_name': 'Demo suite of Weipeng'
+                        # , 'testobject_test_name': 'Demo test of Weipeng'
             , 'name': 'Automation with Sauce labs demo of Android'}
         self.driver = webdriver.Remote('http://us1.appium.testobject.com/wd/hub', desired_caps)
+        sleep(15)
         print(self.driver.desired_capabilities)
 
     def test_launch_and_login(self):
         print('Sleep here a while for now! ')
-        sleep(2)
+        sleep(15)
 
     def tearDown(self):
         self.driver.execute_script('sauce:job-result=passed')
         self.driver.quit()
 
-# ---START OF SCRIPT
+
 if __name__ == '__main__':
     unittest.main()
