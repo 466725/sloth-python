58617bcb4189e7f975a6b1fab6296da9917e9883
Tianle Zheng <tianle.zheng.ca@gmail.com>
2022-07-22 14:32:50 -0400

    This is what I've done so far,  I don't think you can run it though ;P
    What am I supposed to write here? It makes sound now I guess?

--- GoGame/Go game2.py
+++ GoGame/Go game2.py
@@ -2,6 +2,10 @@ import pygame
 import sys
 import time
 
+#soundObj = pygame.mixer.Sound('Move.WAV')
+#soundObj.play()
+
+
 
 def cleanup():
     pygame.quit()
@@ -428,6 +432,8 @@ def main():
         19: "blank",
 
     }
+    global whos_turn
+    whos_turn = "It's the black players turn"
     colour_turn = 1
     white = (255, 255, 255)
     letters = ["A", "B", "C", "D", "E", "F", "G", "H", " I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S"]
@@ -450,6 +456,8 @@ def main():
     else:
         small_side = height
         large_side = width
+    global stone_size
+    stone_size = small_side / 47 + 1
 
     pygame.display.set_caption('Go game board thing')
     screen.fill(brown)
@@ -462,22 +470,36 @@ def main():
     #icon = pygame.image.load('Go.jpg')
     #pygame.display.set_icon(icon)
     is_running = True
+    def draw_stones():
+        print (stone)
 
     def draw_board ():
+        global top_y
+        global left_x
+        global line_length
+
         if height > width:
             small_side = width
             large_side = height
         else:
             small_side = height
             large_side = width
-        global line_length
+
+        top_y = height / 2 - small_side / 21 * 20 / 2
+        left_x = width / 2 - small_side / 21 * 17 / 2
+
         line_length = small_side / 1.165 + 1
+
+        font = pygame.font.Font('freesansbold.ttf', int(height / 33))
+        turn = font.render(whos_turn, False, black)
+        screen.blit(turn, (left_x, top_y + line_length * 1.07))
+
         font = pygame.font.Font('freesansbold.ttf', int(small_side / 33))
         letter_pos = width / 2 - small_side / 21 * 17.02 / 2 + 0.1
         number_pos = height / 2 - small_side / 21 * 17.02 / 2 + 0.1 + (small_side / 1.295)
         numbers_letters = 0
-        xpos = width / 2 - small_side / 21 * 17.02 / 2 + 0.1
-        ypos = height / 2 - small_side / 21 * 20.05 / 2
+        xpos = int(width / 2 - small_side / 21 * 17.02 / 2 + 0.1)
+        ypos = int(height / 2 - small_side / 21 * 20.05 / 2)
 
         pygame.draw.circle(screen, black, (width / 2 - small_side / 21 * 17.02 / 2 + 1.1 + line_length / 19 * 3.15,
                                            height / 2 - small_side / 21 * 20.05 / 2 + 1 + line_length / 19 * 3.18), small_side / 100,
@@ -485,7 +507,7 @@ def main():
         pygame.draw.circle(screen, black, (width / 2 - small_side / 21 * 17.02 / 2 + 1.1 + line_length / 19 * 9.47,
                                            height / 2 - small_side / 21 * 20.05 / 2 + 1 + line_length / 19 * 3.18), small_side / 100,
                            0)
-        pygame.draw.circle(screen, black, (width / 2 - small_side / 21 * 17.02 / 2 + 1.1 + line_length / 19 * 15.78,
+        pygame.draw.circle(screen, black, (width / 2 - small_side / 21 * 17.02 / 2 + 1.1 + line_length / 19 * 15.76,
                                            height / 2 - small_side / 21 * 20.05 / 2 + 1 + line_length / 19 * 3.18), small_side / 100,
                            0)
         pygame.draw.circle(screen, black, (width / 2 - small_side / 21 * 17.02 / 2 + 1.1 + line_length / 19 * 3.15,
@@ -494,7 +516,7 @@ def main():
         pygame.draw.circle(screen, black, (width / 2 - small_side / 21 * 17.02 / 2 + 1.1 + line_length / 19 * 9.47,
                                            height / 2 - small_side / 21 * 20.05 / 2 + 1 + line_length / 19 * 9.47), small_side / 100,
                            0)
-        pygame.draw.circle(screen, black, (width / 2 - small_side / 21 * 17.02 / 2 + 1.1 + line_length / 19 * 15.78,
+        pygame.draw.circle(screen, black, (width / 2 - small_side / 21 * 17.02 / 2 + 1.1 + line_length / 19 * 15.76,
                                            height / 2 - small_side / 21 * 20.05 / 2 + 1 + line_length / 19 * 9.47), small_side / 100,
                            0)
         pygame.draw.circle(screen, black, (width / 2 - small_side / 21 * 17.02 / 2 + 1.1 + line_length / 19 * 3.15,
@@ -503,7 +525,7 @@ def main():
         pygame.draw.circle(screen, black, (width / 2 - small_side / 21 * 17.02 / 2 + 1.1 + line_length / 19 * 9.47,
                                            height / 2 - small_side / 21 * 20.05 / 2 + 1 + line_length / 19 * 15.83), small_side / 100,
                            0)
-        pygame.draw.circle(screen, black, (width / 2 - small_side / 21 * 17.02 / 2 + 1.1 + line_length / 19 * 15.78,
+        pygame.draw.circle(screen, black, (width / 2 - small_side / 21 * 17.02 / 2 + 1.1 + line_length / 19 * 15.76,
                                            height / 2 - small_side / 21 * 20.05 / 2 + 1 + line_length / 19 * 15.83), small_side / 100,
                            0)
 
@@ -512,9 +534,9 @@ def main():
 
         for i in range(19):
             #vertical lines
-            pygame.draw.rect(screen, black, pygame.Rect(xpos, height / 2 - small_side / 21 * 20 / 2, small_side / 350 + 1, line_length))
+            pygame.draw.rect(screen, black, pygame.Rect(xpos, top_y, small_side / 350 + 1, line_length))
             #horizontal lines
-            pygame.draw.rect(screen, black, pygame.Rect(width / 2 - small_side / 21 * 17 / 2, ypos, line_length, small_side / 350 + 1))
+            pygame.draw.rect(screen, black, pygame.Rect(left_x, ypos, line_length, small_side / 350 + 1))
             
 
             letter = letters[numbers_letters]
@@ -522,19 +544,22 @@ def main():
             numbers_letters = numbers_letters + 1
             Letters = font.render(letter, False, black)
             Numbers = font.render(number, False, black)
-            screen.blit(Letters, (letter_pos - 2, height / 2 - small_side / 21 * 17.02 / 2 + 0.1 + (small_side / 1.22)))
+            screen.blit(Letters, (letter_pos - 2, height / 2 - small_side / 21 * 17.02 / 2 + 0.1 + small_side / 1.22))
             screen.blit(Numbers, (width / 2 - small_side / 21 * 17.02 / 2 + 0.1 - small_side / 16.45, number_pos ))
 
-            letter_pos = letter_pos + (small_side / 21.14)
-            number_pos = number_pos - (small_side / 21.14)
+            letter_pos = letter_pos + small_side / 21.14
+            number_pos = number_pos - small_side / 21.14
 
-            xpos = xpos + (small_side / 21)
-            ypos = ypos + (small_side / 21)
+            xpos = xpos + small_side / 21
+            ypos = ypos + small_side / 21
 
     draw_board()
 
 
     while is_running:
+
+        
+        #pygame.draw.circle(screen, black, (300, top_y), 20,0)
 
         #print(line1)
         mouse_pos = pygame.mouse.get_pos()
@@ -544,107 +569,169 @@ def main():
             if event.type == pygame.QUIT:
                 is_running = False
             if event.type == pygame.VIDEORESIZE:
-                width = screen.get_width()
-                height = screen.get_height()
+
+                stone_size = small_side / 47 + 1
+                if event.w < 480:
+                    screen = pygame.display.set_mode((480, event.h), pygame.RESIZABLE)
+                elif event.h < 480:
+                    screen = pygame.display.set_mode((event.w, 480), pygame.RESIZABLE)
+                else:
+                    screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
+
                 if height > width:
                     small_side = width
                     large_side = height
                 else:
                     small_side = height
                     large_side = width
+
+                stone_size = small_side / 47 + 1
+                width = screen.get_width()
+                height = screen.get_height()
+                screen.fill(brown)
+                draw_board()
                 size = event.size
-                if small_side < 480:
-                    small_side = 480
-                    if width > height:
-                        screen = pygame.display.set_mode((width, 480), pygame.RESIZABLE)
-                        screen.fill(brown)
-                        draw_board()
-                    else:
-                        screen = pygame.display.set_mode((480, height), pygame.RESIZABLE)
-                        screen.fill(brown)
-                        draw_board()
-                else:
-                    screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
-                    screen.fill(brown)
-                    draw_board()
+
+
+                #screen.fill(brown)
+                #draw_board()
+
 
 
             if event.type == pygame.MOUSEBUTTONDOWN:
+
+                stone_size = small_side / 47 + 1
+
                 canplay = True
                 colour_turn  = colour_turn + 1
-                if mouse_x > width / 2 - small_side / 21 * 17.02 / 2 + 0.1 - line_length / 20 and mouse_y > height / 2 - small_side / 21 * 20.05 / 2 - line_length / 20 and mouse_x < width / 2 - small_side / 21 * 17.02 / 2 + 0.1 + line_length + line_length / 20 and mouse_y < height / 2 - small_side / 21 * 20.05 / 2 + line_length + line_length / 20:
+                if mouse_x > left_x - line_length / 30 and mouse_y > top_y - line_length / 30 and mouse_x < width / 2 - small_side / 21 * 17.02 / 2 + 0.1 + line_length + line_length / 20 and mouse_y < height / 2 - small_side / 21 * 20.05 / 2 + line_length + line_length / 20:
                     if colour_turn % 2 == 0:
                         colour = "black"
+                        whos_turn = "It's the white players turn"
                     else:
                         colour = "white"
-                    if mouse_y < height / 2 - small_side / 21 * 20 / 2 + line_length / 38:
-                        mouse_y = height / 2 - small_side / 21 * 20 / 2
-                        if mouse_x < 75 and line1[1] == "blank":
-                            mouse_x = 60
-                            line1[1] = colour
-                        elif mouse_x < 105 and line1[2] == "blank" and mouse_x >= 75:
-                            mouse_x = 90
-                            line1[2] = colour
-                        elif mouse_x < 135 and line1[3] == "blank" and mouse_x >= 105:
-                            mouse_x = 120
-                            line1[3] = colour
-                        elif mouse_x < 165 and line1[4] == "blank" and mouse_x >= 135:
-                            mouse_x = 150
-                            line1[4] = colour
-                        elif mouse_x < 195 and line1[5] == "blank" and mouse_x >= 165:
-                            mouse_x = 180
-                            line1[5] = colour
-                        elif mouse_x < 225 and line1[6] == "blank" and mouse_x >= 195:
-                            mouse_x = 210
-                            line1[6] = colour
-                        elif mouse_x < 255 and line1[7] == "blank" and mouse_x >= 225:
-                            mouse_x = 240
-                            line1[7] = colour
-                        elif mouse_x < 285 and line1[8] == "blank" and mouse_x >= 255:
-                            mouse_x = 270
-                            line1[8] = colour
-                        elif mouse_x < 315 and line1[9] == "blank" and mouse_x >= 285:
-                            mouse_x = 300
-                            line1[9] = colour
-                        elif mouse_x < 345 and line1[10] == "blank" and mouse_x >= 315:
-                            mouse_x = 330
-                            line1[10] = colour
-                        elif mouse_x < 375 and line1[11] == "blank" and mouse_x >= 345:
-                            mouse_x = 360
-                            line1[11] = colour
-                        elif mouse_x < 405 and line1[12] == "blank" and mouse_x >= 375:
-                            mouse_x = 390
-                            line1[12] = colour
-                        elif mouse_x < 435 and line1[13] == "blank" and mouse_x >= 405:
-                            mouse_x = 420
-                            line1[13] = colour
-                        elif mouse_x < 465 and line1[14] == "blank" and mouse_x >= 435:
-                            mouse_x = 450
-                            line1[14] = colour
-                        elif mouse_x < 495 and line1[15] == "blank" and mouse_x >= 465:
-                            mouse_x = 480
-                            line1[15] = colour
-                        elif mouse_x < 525 and line1[16] == "blank" and mouse_x >= 495:
-                            mouse_x = 510
-                            line1[16] = colour
-                        elif mouse_x < 555 and line1[17] == "blank" and mouse_x >= 525:
-                            mouse_x = 540
-                            line1[17] = colour
-                        elif mouse_x < 585 and line1[18] == "blank" and mouse_x >= 555:
-                            mouse_x = 570
-                            line1[18] = colour
-                        elif mouse_x < 615 and line1[19] == "blank" and mouse_x >= 585:
-                            mouse_x = 600
-                            line1[19] = colour
-                        else:
-                            canplay = False
-                            screen.blit(wrong_place, (210, 630))
-                            colour_turn = colour_turn - 1
-                    if canplay == True:
-                        pygame.draw.circle(screen, colour, (mouse_x, mouse_y), small_side / 47 + 1, 0)
+                        whos_turn = "It's the black players turn"
+                    #screen.fill(brown)
+                    #draw_board()
 
-        if size != screen.get_size():
-            screen = pygame.display.set_mode(size, pygame.RESIZABLE)
+
+                    line_num = 19 - ((((mouse_y - (top_y + line_length))) / (line_length / 18)) * -1)
+                    if line_num - int(line_num) > 0.5:
+                        line_num = line_num + 1
+                    if line_num < 1:
+                        line_num = 1
+                    elif line_num > 19:
+                        line_num = 19
+
+                    column_num = 19 - ((((mouse_x - (left_x + line_length))) / (line_length / 18)) * -1)
+                    if column_num - int(column_num) > 0.5:
+                        column_num = column_num + 1
+                    if column_num < 1:
+                        column_num = 1
+                    elif column_num > 19:
+                        column_num = 19
+
+                    print(int(line_num), int(column_num))
+                    line_dict = 1
+                    if int(line_num) == 1:
+                        line_dict = line1
+                    elif int(line_num) == 2:
+                        line_dict = line2
+                    elif int(line_num) == 3:
+                        line_dict = line3
+                    elif int(line_num) == 4:
+                        line_dict = line4
+                    elif int(line_num) == 5:
+                        line_dict = line5
+                    elif int(line_num) == 6:
+                        line_dict = line6
+                    elif int(line_num) == 7:
+                        line_dict = line7
+                    elif int(line_num) == 8:
+                        line_dict = line8
+                    elif int(line_num) == 9:
+                        line_dict = line9
+                    elif int(line_num) == 10:
+                        line_dict = line10
+                    elif int(line_num) == 11:
+                        line_dict = line11
+                    elif int(line_num) == 12:
+                        line_dict = line12
+                    elif int(line_num) == 13:
+                        line_dict = line13
+                    elif int(line_num) == 14:
+                        line_dict = line14
+                    elif int(line_num) == 15:
+                        line_dict = line15
+                    elif int(line_num) == 16:
+                        line_dict = line16
+                    elif int(line_num) == 17:
+                        line_dict = line17
+                    elif int(line_num) == 18:
+                        line_dict = line18
+                    elif int(line_num) == 19:
+                        line_dict = line19
+
+                    #canplay = False
+                    #screen.blit(wrong_place, (210, 630))
+                    #colour_turn = colour_turn - 1
+                    mouse_x = left_x + (line_length / 19 * (int(column_num) + 0.05 * int(column_num))) - line_length / 19
+                    mouse_y = top_y + (line_length / 19 * (int(line_num) + 0.05 * int(line_num))) - line_length / 19
+
+                    if line_dict[int(column_num)] != "blank":
+                        canplay = False
+                    else:
+
+                        line_dict[int(column_num)] = colour
+
+                        if int(line_num) == 1:
+                            line1 = line_dict
+                        elif int(line_num) == 2:
+                            line2 = line_dict
+                        elif int(line_num) == 3:
+                            line3 = line_dict
+                        elif int(line_num) == 4:
+                            line4 = line_dict
+                        elif int(line_num) == 5:
+                            line5 = line_dict
+                        elif int(line_num) == 6:
+                            line6 = line_dict
+                        elif int(line_num) == 7:
+                            line7 = line_dict
+                        elif int(line_num) == 8:
+                            line8 = line_dict
+                        elif int(line_num) == 9:
+                            line9 = line_dict
+                        elif int(line_num) == 10:
+                            line10 = line_dict
+                        elif int(line_num) == 11:
+                            line11 = line_dict
+                        elif int(line_num) == 12:
+                            line12 = line_dict
+                        elif int(line_num) == 13:
+                            line13 = line_dict
+                        elif int(line_num) == 14:
+                            line14 = line_dict
+                        elif int(line_num) == 15:
+                            line15 = line_dict
+                        elif int(line_num) == 16:
+                            line16 = line_dict
+                        elif int(line_num) == 17:
+                            line17 = line_dict
+                        elif int(line_num) == 18:
+                            line18 = line_dict
+                        elif int(line_num) == 19:
+                            line19 = line_dict
+
+                    if canplay == True:
+                        pygame.mixer.music.load('move.wav')
+                        pygame.mixer.music.play()
+                        pygame.draw.circle(screen, colour, (mouse_x, mouse_y), stone_size, 0)
+                    else:
+                        colour_turn = colour_turn - 1
+
+        #if size != screen.get_size():
+            #screen = pygame.display.set_mode(size, pygame.RESIZABLE)
 
         pygame.display.flip()
 
