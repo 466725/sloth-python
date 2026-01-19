d7b6c34028aa695f0d65de2cb52982385622b185
Weipeng Zheng <weipeng.zheng.ca@gmail.com>
2026-01-19 18:25:47 -0500

    Add Student class with initialization,

---
+++ tutorial/EducationManagement/Student.py
@@ -0,0 +1,15 @@
+"""
+Student class
+"""
+class Student:
+    def __init__(self, name, age, score):
+        self.name = name
+        self.age = age
+        self.score = score
+
+    def __str__(self):
+        return f"Student(name='{self.name}', age={self.age}, score={self.score})"
+
+    def update_score(self, score=None):
+        if score is not None:
+            self.score = score
