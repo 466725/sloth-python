06289d21d67e43f87286d1c986154e011a93171c
Weipeng Zheng <weipeng.zheng.ca@gmail.com>
2026-02-05 14:20:54 -0500

    fix logger related warnings
    fix logger related warnings

--- pytest_demo/tests/ui/amazon/amazon_signinpage_test.py
+++ pytest_demo/tests/ui/amazon/amazon_signinpage_test.py
@@ -3,14 +3,16 @@ import logging
 import pytest
 from selenium.webdriver.common.by import By
 
-from utils.constants import SELENIUM_IMPLICITLY_WAIT, SLEEP_TIME
+from utils.constants import SELENIUM_IMPLICITLY_WAIT
 
 logger = logging.getLogger(__name__)
+account_list_link = None
 
 
 def goto_register_page(open_homepage):
     logger.info("Navigating to Amazon register page")
-    open_homepage.find_element(By.ID, "nav-link-accountList").click()
+    account_list_link = open_homepage.find_element(By.ID, "nav-link-accountList")
+    account_list_link.click()
     open_homepage.implicitly_wait(SELENIUM_IMPLICITLY_WAIT)
     open_homepage.find_element(By.ID, "ab-registration-ingress-link").click()
     open_homepage.implicitly_wait(SELENIUM_IMPLICITLY_WAIT)
@@ -22,6 +24,7 @@ def goto_register_page(open_homepage):
 def test_signinpage_title(open_homepage):
     logger.info("Verifying signin page title")
     signin_page = open_homepage
-    signin_page.find_element(By.ID, "nav-link-accountList").click()
+    account_list_link = signin_page.find_element(By.ID, "nav-link-accountList")
+    account_list_link.click()
     signin_page.implicitly_wait(SELENIUM_IMPLICITLY_WAIT)
     assert "Amazon Sign-In" in signin_page.title
