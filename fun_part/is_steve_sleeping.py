494b2e54712036bee08aaaf89a694aedc862af57
Weipeng Zheng <weipeng.zheng.ca@gmail.com>
2026-04-12 15:57:16 -0400

    Update is_steve_sleeping.py

--- fun_part/is_steve_sleeping.py
+++ fun_part/is_steve_sleeping.py
@@ -1,17 +1,19 @@
 # How can you ask a user for information?
 # How can you include if and else into a question
-for x in range(6):
-    print("Is Steve sleeping")
-    steveSleeping = str(input())
-    if steveSleeping == "yes":
-        #        :(
-        print("Steve woke up")
-    elif steveSleeping == "no":
-        #         :)
-        print("Steve went to sleep")
-    elif steveSleeping == "AHH":
-        #        :(
-        print("Steve woke up")
-    else:
-        #              :|
-        print("Steve did not respond")
+def ask_steve_status():
+    for _ in range(6):
+        while True:
+            response = input("Is Steve sleeping? (yes/no): ").strip().lower()
+
+            if response in ("yes", "no", "ahh"):
+                break
+            print("Please enter yes or no.")
+
+        if response in ("yes", "ahh"):
+            print("Steve woke up 😢")
+        else:
+            print("Steve went to sleep 🙂")
+
+
+if __name__ == "__main__":
+    ask_steve_status()
