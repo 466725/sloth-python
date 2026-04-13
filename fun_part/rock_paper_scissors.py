efc1b1ec4c6e30502225b9c8a81010f02db46617
Weipeng Zheng <weipeng.zheng.ca@gmail.com>
2026-04-13 09:01:45 -0400

    Update rock_paper_scissors.py

--- fun_part/rock_paper_scissors.py
+++ fun_part/rock_paper_scissors.py
@@ -1,108 +1,90 @@
-# Things you may need.
-Q1 = str(input("Do you wish to play against a mcu, another player or the hacker"))
-if Q1 == "mcu":
-    user = str(input("choose between rock paper and scissors:"))
-    import random
+import random
 
-    Thislist = ["scissors", "rock", "paper"]
-    a = random.randint(0, 3)
-    mcu = Thislist[a]
-    if mcu == user:
-        print(user, "!!!")
-        print(mcu, "!!!")
+CHOICES = ("rock", "paper", "scissors")
+
+
+def get_choice(prompt):
+    while True:
+        choice = input(prompt).strip().lower()
+        if choice in CHOICES:
+            return choice
+        print("Invalid choice. Please enter rock, paper, or scissors.")
+
+
+def determine_winner(p1, p2):
+    if p1 == p2:
+        return "tie"
+    wins = {
+        ("rock", "scissors"),
+        ("scissors", "paper"),
+        ("paper", "rock"),
+    }
+    return "p1" if (p1, p2) in wins else "p2"
+
+
+def play_vs_computer():
+    user = get_choice("Choose rock, paper, or scissors: ")
+    computer = random.choice(CHOICES)
+
+    print(f"You: {user} !!!")
+    print(f"Computer: {computer} !!!")
+
+    result = determine_winner(user, computer)
+    if result == "tie":
         print("You tied!!!")
-    elif mcu == "scissors" and user == "rock":
-        print(user, "!!!")
-        print(mcu, "!!!")
+    elif result == "p1":
         print("You won!!!")
-    elif mcu == "rock" and user == "paper":
-        print(user, "!!!")
-        print(mcu, "!!!")
-        print("You won!!!")
-    elif mcu == "paper" and user == "scissors":
-        print(user, "!!!")
-        print(mcu, "!!!")
-        print("You won!!!")
-    elif mcu == "rock" and user == "scissors":
-        print(user, "!!!")
-        print(mcu, "!!!")
-        print("You lost!!!")
-    elif mcu == "scissors" and user == "paper":
-        print(user, "!!!")
-        print(mcu, "!!!")
-        print("You lost!!!")
-    elif mcu == "paper" and user == "rock":
-        print(user, "!!!")
-        print(mcu, "!!!")
-        print("You lost!!!")
     else:
-        print("You lost!!!!!")
-# You should know how to use import
-# You should know how to use input
-# You should know how to use booleans
-# You should understand conditionas
-# You should know how to use print
-# You should know how to use loops
-# You should know how to use return
-elif Q1 == "another player":
-    user1 = str(input("choose between rock paper and scissors:"))
-    print("YOU LOST!!!!!!!!!!!!!!!")
-    print("")
-    print("")
-    print("")
-    print("")
-    print("Dont look")
-    print(
-        "============================================================================================================================================================================================================================================="
-    )
-    print("Dont look")
-    print("")
-    print("")
-    print("")
-    print("")
-    user2 = str(input("choose between rock paper and scissors( you too ):"))
-    if user1 == user2:
-        print(user1, "!!!")
-        print(user2, "!!!")
+        print("You lost!!!")
+
+
+def play_vs_player():
+    user1 = get_choice("Player 1: choose rock, paper, or scissors: ")
+
+    print("\n" * 20)  # simple screen clear
+
+    user2 = get_choice("Player 2: choose rock, paper, or scissors: ")
+
+    print(f"Player1: {user1} !!!")
+    print(f"Player2: {user2} !!!")
+
+    result = determine_winner(user1, user2)
+    if result == "tie":
         print("You two tied!!!")
-    elif user1 == "scissors" and user2 == "rock":
-        print(user1, "!!!")
-        print(user2, "!!!")
-        print("User2 won!!!")
-    elif user1 == "rock" and user2 == "paper":
-        print(user1, "!!!")
-        print(user2, "!!!")
-        print("User2 won!!!")
-    elif user1 == "paper" and user2 == "scissors":
-        print(user1, "!!!")
-        print(user2, "!!!")
-        print("User2 won!!!")
-    elif user1 == "rock" and user2 == "scissors":
-        print(user1, "!!!")
-        print(user2, "!!!")
-        print("User1 won!!!")
-    elif user1 == "scissors" and user2 == "paper":
-        print(user1, "!!!")
-        print(user2, "!!!")
-        print("User1 won!!!")
-    elif user1 == "paper" and user2 == "rock":
-        print(user1, "!!!")
-        print(user2, "!!!")
-        print("User1 won!!!")
+    elif result == "p1":
+        print("Player1 won!!!")
     else:
-        print("You lost!!!!!")
-elif Q1 == "The hacker":
-    for i in range(100):
-        H = 0
-        user = str(input("choose between rock paper and scissors:"))
-        if user == "scissors":
-            H = "rock"
-        elif user == "rock":
-            H = "paper"
-        elif user == "paper":
-            H = "scissors"
-        print(user + "!!!")
-        print(H + "!!!")
-        print("You lost!!!")
+        print("Player2 won!!!")
+
+
+def play_vs_hacker():
+    user = get_choice("Choose rock, paper, or scissors: ")
+
+    # Hacker always wins 😈
+    counter = {
+        "rock": "paper",
+        "paper": "scissors",
+        "scissors": "rock",
+    }
+    hacker = counter[user]
+
+    print(f"You: {user} !!!")
+    print(f"Hacker: {hacker} !!!")
+    print("You lost!!!")
+
+
+def main():
+    mode = input("Play against computer, another player, or hacker? ").strip().lower()
+
+    if mode == "computer":
+        play_vs_computer()
+    elif mode == "another player":
+        play_vs_player()
+    elif mode == "hacker":
+        play_vs_hacker()
     else:
-        print("You lost!!!!!")
+        print("Invalid mode.")
+
+
+if __name__ == "__main__":
+    main()
