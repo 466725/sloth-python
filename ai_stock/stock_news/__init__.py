3c94f9c5069b0544802717d138fcc5a9d767ae16
Weipeng Zheng <weipeng.zheng.ca@gmail.com>
2026-07-13 14:38:29 -0400

    Update __init__.py

--- ai_stock/stock_news/__init__.py
+++ ai_stock/stock_news/__init__.py
@@ -1,3 +1,14 @@
 from .ai_stock_news_agent import AIStockNewsAgent, StockNewsContext
+from .alphavantage_news_fetcher import AlphaVantageNewsFetcher
+from .finnhub_news_fetcher import FinnhubNewsFetcher
+from .provider import CompositeNewsProvider
+from .yfinance_news_fetcher import YFinanceNewsFetcher
 
-__all__ = ["AIStockNewsAgent", "StockNewsContext"]
+__all__ = [
+	"AIStockNewsAgent",
+	"StockNewsContext",
+	"CompositeNewsProvider",
+	"FinnhubNewsFetcher",
+	"AlphaVantageNewsFetcher",
+	"YFinanceNewsFetcher",
+]
