0c3180a9def7265577e785a730e1e843f48edb4b
Weipeng Zheng <weipeng.zheng.ca@gmail.com>
2026-09-18 10:13:15 -0400

    Update tangerine_support.py

--- pytest/ui/tangerine_support.py
+++ pytest/ui/tangerine_support.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import logging
 from typing import Any
 
-from utils.config import settings
+from config.config import settings
 
 logger = logging.getLogger(__name__)
 
