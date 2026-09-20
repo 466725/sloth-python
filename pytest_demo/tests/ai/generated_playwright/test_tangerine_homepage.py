657de8978388a1f45df0dcfb0307f081a65bcae6
Weipeng Zheng <weipeng.zheng.ca@gmail.com>
2026-09-20 09:53:55 -0400

    Delete test_tangerine_homepage.py

--- pytest_demo/tests/ai/generated_playwright/test_tangerine_homepage.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from playwright.sync_api import Page
-
-def test_tangerine_homepage(page: Page):
-    page.goto("https://www.tangerine.ca/en/personal")
-    assert page.get_by_text("Log In").is_visible()
-    assert page.get_by_text("Become a Client").is_visible()
