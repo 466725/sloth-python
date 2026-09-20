709e0c911d8ded3bf67caa0fc0d09c903fe24497
Weipeng Zheng <weipeng.zheng.ca@gmail.com>
2026-09-20 19:43:10 -0400

    Update test_signuppage.py

--- pytest_tests/ui/tangerine/test_signuppage.py
+++ pytest_tests/ui/tangerine/test_signuppage.py
@@ -1,5 +1,6 @@
 import logging
 
+import allure
 import pytest
 
 from ui.tangerine.test_signinpage import goto_signup_page
@@ -9,6 +10,7 @@ logger = logging.getLogger(__name__)
 
 @pytest.mark.ui
 @pytest.mark.playwright
+@allure.description("Owner: Weipeng Zheng")
 def test_signuppage_title(tangerine_homepage):
     logger.info("Verifying Tangerine signup page title (Playwright)")
     signup_page = goto_signup_page(tangerine_homepage)
